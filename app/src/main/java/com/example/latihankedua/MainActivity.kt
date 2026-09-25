bae866261df69374f59ddfc71d362e7724ea6ab5
rivanpamungkas <rivan.pamungkas.core@gmail.com>
2026-09-26 01:25:02 +0700

    Belajar Tipe Data dan variabel

--- app/src/main/java/com/example/latihankedua/MainActivity.kt
+++ app/src/main/java/com/example/latihankedua/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.latihankedua
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +17,28 @@ import com.example.latihankedua.ui.theme.LatihanKeduaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val namaToko: String = "Warung BU Yanti"
+        var hargaProduk: Double = 15000.0
+        var stokBarang: Int = 25
+        val tokoBuku: Boolean = true
+
+        Log.d("KatalogUMKM", "Nama Toko: $namaToko")
+        Log.d("KatalogUmkm", "Harga Produk: $hargaProduk")
+        Log.d("KatalogUmkm", "Stok Barang: $stokBarang")
+        Log.d("KatalohUmkm", "Status Buka: $tokoBuku")
+
+        Toast.makeText(this, "Selamat datang di $namaToko", Toast.LENGTH_LONG).show()
+
+        val kategoriProduk: String = "Makanan Ringan"
+        Log.d("KatalogUmkm", "Kategori Produk: $kategoriProduk")
+
+        stokBarang = 18
+        Log.d("KatalogUmkm", "Stok Barang Terbaru: $stokBarang")
+
+        val kotaAsal = "Batusangkar"
+
+        kotaAsal = 12345
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
